23a1f7bf818c2864a19d765e6a2e923785f309a2
HP <HP@DESKTOP-VP5V6ES>
2023-02-20 10:36:39 +0200

    Add project files.

---
+++ ADOExample/Data/Migrations/20220525074210_add_AdoExample_db.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ADOExample.Data.Migrations
+{
+    public partial class add_AdoExample_db : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ADOExamples",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(nullable: true),
+                    Price = table.Column<string>(nullable: true),
+                    Qty = table.Column<string>(nullable: true),
+                    Remarks = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ADOExamples", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ADOExamples");
+        }
+    }
+}
---
+++ ADOExample/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADOExample.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Price { get; set; }
+        public string Qty { get; set; }
+        public string Remarks { get; set; }
+    }
+}
